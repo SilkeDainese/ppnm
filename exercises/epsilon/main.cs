fe30ffe23de50ef460bf9b421c34f476e51d3a05
SilkeDainese <silke.dainese@gmail.com>
2022-03-04 14:19:34 +0100

    Make folders

---
+++ exercises/epsilon/main.cs
@@ -0,0 +1,10 @@
+using System;
+using static System.Console;
+class main{
+public static void Main(){
+	//int i=int.MaxValue/10*9;
+	int i=1;
+	while(i+1>i) {i++;}
+	Write("my max int = {0]\n",i);
+}
+}
