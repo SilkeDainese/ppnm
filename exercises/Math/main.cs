6f35766e04a4edac3afdd442859b7487615c426b
SilkeDainese <silke.dainese@gmail.com>
2022-03-25 19:14:46 +0100

    Epsilon

--- exercises/Math/main.cs
+++ exercises/Math/main.cs
@@ -1,30 +1,15 @@
-using System;
+using static System.Math;
 using static System.Console;
 
-public static class test
-{
-  public static int n = 7;
-  public static double pi = 3.1415927;
-  public static double e = 2.71828;
-}
+static public class math {
+	static void Main() {
+		double sqrt2 = Sqrt(2.0);
+		double epi = Pow(E, PI);
+		double pie = Pow(PI, E);
+		Write($"sqrt(2) = {sqrt2}\n");
+		Write($"sqrt2*sqrt2 = {sqrt2*sqrt2} (should be equal to 2)\n");
+		Write($"e^pi = {epi}\n");
+		WriteLine($"pi^e = {pie}\n");
+	}
 
-public static class main {
-  //static string s = "main\n";
-  static void hello ()
-  {
-    string s = "hello\n";
-      Write (s);
-    {
-      string s1 = "block\n";
-        Write (s1);
-  }}
-static int Main() {
-    double x, y;
-      x = test.pi;
-      y = test.e;
-      Write ("x={0} y={1}\n", x, y);
-      Write ($"x={x} y={y}\n");
-	math.test();
-      return 0;
-  }
 }
--- exercises/epsilon/main.cs
+++ exercises/epsilon/main.cs
@@ -1,10 +1,20 @@
-using System;
 using static System.Console;
-class main{
-public static void Main(){
-	//int i=int.MaxValue/10*9;
-	int i=1;
-	while(i+1>i) {i++;}
-	Write("my max int = {0]\n",i);
-}
+using static System.Math;
+
+static class epsilon{
+	static void Main(){
+		Write("Excerise 1: \n");
+		int i=1;
+		while(i+1>i) {i++;}
+		Write($"My max int is {i}\n");
+		Write($"int.MaxValue is {int.MaxValue}\n");
+		
+		int j = 1;
+		while(j-1<j) {j--;}
+		Write($"My min int is {j}\n");
+		Write($"int.MinValue is {int.MinValue}\n");
+
+		Write("Exercise 2");
+
+	}
 }
