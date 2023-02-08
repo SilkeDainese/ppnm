281a1e371fdb34b9c3b82b3bfe19d11a47a9ec51
SilkeDainese <silkedainese@DESKTOP-VTQ9D9J>
2023-02-08 09:19:25 +0100

    C-charp excercise

--- exercises/Math/main.cs
+++ exercises/Math/main.cs
@@ -4,12 +4,12 @@ using static System.Console;
 static public class math {
 	static void Main() {
 		double sqrt2 = Sqrt(2.0);
+		double two1_5 = Pow(2,0.2);
 		double epi = Pow(E, PI);
 		double pie = Pow(PI, E);
 		Write($"sqrt(2) = {sqrt2}\n");
 		Write($"sqrt2*sqrt2 = {sqrt2*sqrt2} (should be equal to 2)\n");
 		Write($"e^pi = {epi}\n");
 		WriteLine($"pi^e = {pie}\n");
-	}
-
+}
 }
---
+++ exercises/csharp/main1.cs
@@ -0,0 +1,9 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+class main{
+	public static double Main(){
+	Write("hello\n");
+	}
+}
