0fc4401539847a39d320f0f815123e74d8f397d9
SilkeDainese <silkedainese@DESKTOP-PSA7527.localdomain>
2023-08-13 14:55:13 +0200

    Plots exercise

--- exercises/plots2/main.cs
+++ exercises/plots2/main.cs
@@ -7,23 +7,23 @@ class main{
 
 	static void Main(){
 
-		// Taking data from Wiki-table
+		// Data taken from Wikipedia tabæe
 
-		char[] split_delimiters = {' ','\t','\n'};
+		char[] split_delimiters = {'\t','\n',' '};
 		var split_options = StringSplitOptions.RemoveEmptyEntries;
 
-		vector x = new vector(32);  // x values
-		vector y1 = new vector(32); // erf(x)
-		vector y2 = new vector(32); // 1 - erf(x)
+		vector x = new vector(32); // x-values
+		vector y = new vector(32); // erf(x)
+		vector z = new vector(32); // 1 - erf(x)
 
-		int s = 0;
+		int n = 0;
 		for( string line = ReadLine(); line != null; line = ReadLine() ){
 
 			var numbers = line.Split(split_delimiters,split_options);
-			x[s] = double.Parse(numbers[0]);
-			y1[s] = double.Parse(numbers[1]);
-			y2[s] = double.Parse(numbers[2]);
-			s += 1;
+			x[n] = double.Parse(numbers[0]);
+			y[n] = double.Parse(numbers[1]);
+			z[n] = double.Parse(numbers[2]);
+			n += 1;
         	}
 
 		// Taking data from Wiki-table
@@ -33,16 +33,15 @@ class main{
 		for(int i = 0; i< 32;i++){
 
 			erf = sfun.erf(x[i]);
-
-			WriteLine($"{x[i]} {y1[i]} {erf}");
+			WriteLine($"{x[i]} {y[i]} {erf}");
 
 		}
-		WriteLine(); WriteLine();
+		WriteLine();
 
 		double gamma;
 		double lngamma;
 
-		double x1; // x value itteration
+		double xs; // x value itteration
 		int N = 100; // number of points in interval
 
 		int a = 1; // start of interval
@@ -50,22 +49,21 @@ class main{
 
 		for(int i = 0; i < N; i++){
 
-			x1 = a + (b-a)*i/(N-1);
+			xs = a + (b-a)*i/(N-1);
 
-			gamma = sfun.gamma(x1);
-			lngamma = sfun.lngamma(x1);
+			gamma = sfun.gamma(xs);
+			lngamma = sfun.lngamma(xs);
 			
 
-			WriteLine($"{x1} {gamma} {lngamma}");
+			WriteLine($"{xs} {gamma} {lngamma}");
 
 		}
-		WriteLine(); WriteLine();
+		WriteLine();
 
-		var fact = new double[b]; // factorial 
+		var fact   = new double[b]; // factorial 
 		var lnfact = new double[b]; // logarithm of factorial
 
-		for(int i = 0; i < b; i++){ // some descrete analytical values of the gamma function (the factorials)
-		
+		for(int i = 0; i < b; i++){		
 			fact[i] = 1;
 			for(int j = 1; j <= i+1; j++){
 
