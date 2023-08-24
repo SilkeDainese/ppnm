fe3932a25ec82242f6fdc224d6345d8f6890be76
SilkeDainese <silkedainese@DESKTOP-PSA7527.localdomain>
2023-08-24 15:23:47 +0200

    homework final

--- homeworks/eigenValues/taskA/main.cs
+++ homeworks/eigenValues/taskA/main.cs
@@ -1,62 +1,64 @@
 using System;
 using static System.Console;
 using static System.Math;
-using static System.Random;
+using Random = System.Random;
 
 public class main{
 
-	public static void Main(){
-		int n = 3;
-		int m = n;
-		matrix A = new matrix(n,m);
-		matrix V = new matrix(n,m);
-		var rand = new Random();
-		for(int i=0; i<n; i++){
-			for(int j=0; j<m; j++){
-				double number = rand.NextDouble();
-				A[i,j] = number; //Hermitisk matrix
-				A[j,i] = number;
-			}
-		}
-		matrix D = A.copy();
-		WriteLine($"Random generated {n} by {m} matrix");
-		A.print();
-		
-		WriteLine($"\n");
-		
-		int sweeps = jacobev.JacobCyclic(D,V);
-		WriteLine($"Jacob eigenvalue diagonalization of A. Number of sweeps={sweeps}");
-		WriteLine($"Matrix D, with eigenvalues on the diagonal:");
-		D.print();
-		
-		WriteLine($"\n");
-		
-		WriteLine($"Checking that V^T*A*V=D");
-		matrix V_trans_A_V = V.transpose()*A*V;
-		V_trans_A_V.print();
-		WriteLine($"{V_trans_A_V.approx(D)}");
+    public static void Main() {
+        int matrixSize = 3;
+        int dimension = matrixSize;
+        matrix A = new matrix(matrixSize, dimension);
+        matrix V = new matrix(matrixSize, dimension);
+        var randomGenerator = new Random();
 
-		WriteLine($"\n");
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < dimension; j++) {
+                double randomNumber = randomGenerator.NextDouble();
+                A[i, j] = randomNumber; 
+                A[j, i] = randomNumber;
+            }
+        }
 
-		WriteLine($"Checking that V*D*V^T=A");
-		matrix V_D_V_trans = V*D*V.transpose();
-		WriteLine($"{V_D_V_trans.approx(A)}");
-		V_D_V_trans.print();	
-	
-		WriteLine($"\n");
+        matrix D = A.copy();
+        WriteLine($"Randomly generated {matrixSize} by {dimension} matrix");
+        A.print();
 
-		WriteLine($"Checking that V^T*V=I");
-		matrix V_trans_V = V.transpose()*V;
-		matrix I = new matrix(n,n);
-		I.set_unity();
-		WriteLine($"{V_trans_V.approx(I)}");
-		V_trans_V.print();
-	
-		WriteLine($"\n");
-	
-		WriteLine($"Checking that V*V^T=I");
-		matrix V_V_trans = V*V.transpose();
-		WriteLine($"{V_V_trans.approx(I)}");
-		V_V_trans.print();
-	}
+        WriteLine($"\n");
+
+        int sweepCount =jacobev.JacobCyclic(D,V); 
+        WriteLine($"Eigenvalue diagonalization of A using Jacobi method. Number of sweeps = {sweepCount}");
+        WriteLine($"Matrix D, with eigenvalues on the diagonal:");
+        D.print();
+
+        WriteLine($"\n");
+
+        WriteLine($"Verifying that V^T * A * V = D");
+        matrix VTransAV = V.transpose() * A * V;
+        VTransAV.print();
+        WriteLine($"{VTransAV.approx(D)}");
+
+        WriteLine($"\n");
+
+        WriteLine($"Verifying that V * D * V^T = A");
+        matrix VDVT = V * D * V.transpose();
+        VDVT.print();
+        WriteLine($"{VDVT.approx(A)}");
+
+        WriteLine($"\n");
+
+        WriteLine($"Verifying that V^T * V = I");
+        matrix VTransV = V.transpose() * V;
+        matrix identity = new matrix(matrixSize, matrixSize);
+        identity.set_unity();
+        WriteLine($"{VTransV.approx(identity)}");
+        VTransV.print();
+
+        WriteLine($"\n");
+
+        WriteLine($"Verifying that V * V^T = I");
+        matrix VVTrans = V * V.transpose();
+        WriteLine($"{VVTrans.approx(identity)}");
+        VVTrans.print();
+    }
 }
